b94cb58783a5a8bbb7c3e23df7a9d66c462b61a5
Sowad <sowadhossain017@gmail.com>
2023-08-02 07:02:29 +0600

    added Exercise problem3

---
+++ CSE115L/Lab3/Exercise_prob3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.1416
+int main(){
+	float Radius;
+	printf("Radius of the Sphere :");
+	scanf("%f",&Radius);
+	printf("Volum of the Sphere : %.2f",(3.0/4)*PI*pow(Radius,3));
+
+}
